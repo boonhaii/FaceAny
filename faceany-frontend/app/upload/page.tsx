c7fcbb2daa43133d75e819bf459e9e6143e727dc
Jeremy Hon <jeremy@staffany.com>
2023-06-12 13:44:37 +0800

    user selection for upload

--- faceany-frontend/app/upload/page.tsx
+++ faceany-frontend/app/upload/page.tsx
@@ -1,20 +1,61 @@
 "use client";
+
+import { useCallback, useState } from "react";
+import { WebcamComponent } from "../../webcam";
+
 export default function Upload() {
+  const [result, setResult] = useState("");
+  const [user, setUser] = useState<string | null>(null);
+
+  const handleUpload = useCallback(
+    async (src: string) => {
+      if (!user) {
+        return;
+      }
+      const file = await urltoFile(src, user + ".jpg", "image/jpeg");
+      console.log("file constructed");
+      const success = await uploadPhoto(file, user);
+      if (success) {
+        setResult("Photo uploaded successfully!");
+        return;
+      }
+      setResult("Upload failed");
+    },
+    [user]
+  );
+
   return (
-    <>
-      <p>Upload a .png or .jpg image (max 1MB).</p>
-      <input
-        onChange={uploadPhoto}
-        type="file"
-        accept="image/png, image/jpeg"
-      />
-    </>
+    <div className="flex p-10">
+      <div className="flex-1">
+        <p>User Selection</p>
+        <select
+          className="bg-black"
+          onChange={(e) => {
+            setUser(e.target.value);
+            setResult("");
+          }}
+        >
+          <option value="">Select a user</option>
+          <option value="Boon Hai">Boon Hai</option>
+          <option value="Jeremy">Jeremy</option>
+          <option value="Vanessa">Vanessa</option>
+        </select>
+      </div>
+      <div className="flex-1">
+        {user ? (
+          <>
+            <p>Snap a photo for {user}!</p>
+            <WebcamComponent onCapture={handleUpload} />
+            <p>{result}</p>
+          </>
+        ) : null}
+      </div>
+    </div>
   );
 }
 
-const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  const file = e.target.files?.[0]!;
-  const filename = encodeURIComponent(file.name);
+const uploadPhoto = async (file: File, user: string) => {
+  const filename = encodeURIComponent(user + ".jpg");
   const fileType = encodeURIComponent(file.type);
 
   const res = await fetch(
@@ -31,14 +72,33 @@ const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
     method: "POST",
     body: formData,
   });
+
   if (upload.ok) {
-    console.log("Uploaded successfully!");
     const indexFaceResponse = await fetch(
       "https://smh7spdpmi.execute-api.ap-southeast-1.amazonaws.com/setup",
-      { method: "POST", body: JSON.stringify({ imageFileName: `${filename}` }) }
+      { method: "POST", body: JSON.stringify({ imageFileName: filename }) }
     );
-    console.log(indexFaceResponse);
-  } else {
-    console.error("Upload failed.");
+    if (indexFaceResponse.status === 200) {
+      return upload.ok;
+    }
   }
+  console.error("Upload failed.");
 };
+
+function urltoFile(url: string, filename: string, mimeType: string) {
+  if (url.startsWith("data:")) {
+    var arr = url.split(","),
+      mime = arr[0].match(/:(.*?);/)![1],
+      bstr = atob(arr[arr.length - 1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    var file = new File([u8arr], filename, { type: mime || mimeType });
+    return Promise.resolve(file);
+  }
+  return fetch(url)
+    .then((res) => res.arrayBuffer())
+    .then((buf) => new File([buf], filename, { type: mimeType }));
+}
---
+++ faceany-frontend/app/webcam.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import Webcam from "react-webcam";
+
+interface Props {
+  onCapture: (src: string) => void;
+}
+
+export const WebcamComponent = ({ onCapture }: Props) => {
+  const webcamRef = useRef<Webcam>(null);
+  const [imgSrc, setImgSrc] = useState<string | null>(null);
+
+  const capture = useCallback(() => {
+    if (!webcamRef.current) {
+      return;
+    }
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (imageSrc) {
+      setImgSrc(imageSrc);
+      onCapture(imageSrc);
+    }
+  }, [webcamRef, onCapture]);
+
+  return (
+    <div className="flex-col flex">
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        mirrored={true}
+      />
+      <div className="flex flex-1 justify-center p-5">
+        <button className="px-3 rounded-full bg-slate-700" onClick={capture}>
+          Capture photo
+        </button>
+      </div>
+      {imgSrc && <img src={imgSrc} />}
+    </div>
+  );
+};
