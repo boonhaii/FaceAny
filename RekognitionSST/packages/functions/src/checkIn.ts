84cfadc39e91d24f61dc086ee35aadaab52c99e0
boonhaii <boonhaii.dev@gmail.com>
2023-06-12 11:39:18 +0800

    Switch handler to search from collection instead

--- RekognitionSST/packages/functions/src/checkIn.ts
+++ RekognitionSST/packages/functions/src/checkIn.ts
@@ -1,36 +1,62 @@
 import {
   RekognitionClient,
   CompareFacesCommand,
+  SearchFacesByImageCommand,
+  Face,
 } from "@aws-sdk/client-rekognition";
 import middy from "@middy/core";
 import httpMultipartBodyParser from "@middy/http-multipart-body-parser";
 import { ApiHandler } from "sst/node/api";
 
-async function callAWSCompareFaces(
+import { identityDatabase } from "./identityDatabase";
+
+function mapFaceIdToPerson(faceId: string) {
+  return identityDatabase[faceId];
+}
+
+async function callAWSSearchCollection(
   client: RekognitionClient,
   targetImage: Buffer
 ) {
-  const command = new CompareFacesCommand({
-    QualityFilter: "AUTO", // Whether filter is applied, default is none.
-    SimilarityThreshold: 70, // Minimal Threshold that must be met indicate same face.
-    // Probably will need them to setup with image uploaded to the bucket.
-    SourceImage: {
-      S3Object: {
-        Bucket: "hackany",
-        Name: "sourceImg.jpg",
-      },
-    },
-    // Probably using Bytes, we just take the image from the frontend, then send the bytes in.
-    TargetImage: {
+  const command = new SearchFacesByImageCommand({
+    CollectionId: "faceAnyUsers",
+    FaceMatchThreshold: 70,
+    Image: {
       Bytes: targetImage,
     },
+    MaxFaces: 1, // Number of matched faces to return.
+    QualityFilter: "AUTO",
   });
 
   const data = await client.send(command);
-  // FaceMatches is an array of faces in the targetImage that matches the sourceImg
-  // And the similarity is above the similarityThreshold passed.
   return data;
 }
+
+// async function callAWSCompareFaces(
+//   client: RekognitionClient,
+//   targetImage: Buffer
+// ) {
+//   const command = new CompareFacesCommand({
+//     QualityFilter: "AUTO", // Whether filter is applied, default is none.
+//     SimilarityThreshold: 70, // Minimal Threshold that must be met indicate same face.
+//     // Probably will need them to setup with image uploaded to the bucket.
+//     SourceImage: {
+//       S3Object: {
+//         Bucket: "hackany",
+//         Name: "sourceImg.jpg",
+//       },
+//     },
+//     // Probably using Bytes, we just take the image from the frontend, then send the bytes in.
+//     TargetImage: {
+//       Bytes: targetImage,
+//     },
+//   });
+
+//   const data = await client.send(command);
+//   // FaceMatches is an array of faces in the targetImage that matches the sourceImg
+//   // And the similarity is above the similarityThreshold passed.
+//   return data;
+// }
 
 const logicHandler = ApiHandler(async (event) => {
   const eventBody = event.body as unknown as {
@@ -49,20 +75,29 @@ const logicHandler = ApiHandler(async (event) => {
 
   const client = new RekognitionClient({ region: "ap-southeast-1" });
   try {
-    const compareResult = await callAWSCompareFaces(client, targetImageBytes);
-    const faceMatches = compareResult.FaceMatches;
-    if (faceMatches && faceMatches.length !== 0) {
+    // const compareResult = await callAWSCompareFaces(client, targetImageBytes);
+    // const faceMatches = compareResult.FaceMatches;
+    const searchCollectionResult = await callAWSSearchCollection(
+      client,
+      targetImageBytes
+    );
+    const faceMatches = searchCollectionResult.FaceMatches;
+    if (!faceMatches || faceMatches.length === 0) {
       return {
-        statusCode: 200,
+        statusCode: 204,
         body: JSON.stringify({
-          message: "Found a matching face, check in successful.",
+          message: "Did not find any matching faces, please try again!",
         }),
       };
     }
+
+    const faceObject = faceMatches[0].Face as unknown as Face;
+    const faceId = faceObject.FaceId as string;
+    const person = mapFaceIdToPerson(faceId);
     return {
-      statusCode: 204,
+      statusCode: 200,
       body: JSON.stringify({
-        message: "Did not find a matching face, please try again!",
+        message: `Check in successful for user: ${person}.`,
       }),
     };
   } catch (e) {
